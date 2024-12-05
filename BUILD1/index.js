98c856c7c5c271b3ae171dc8eb41660e1eaed3d5
Vicky Stephens <adastephens91@gmail.com>
2024-12-05 16:47:44 +0100

    all changes saved

---
+++ BUILD1/index.js
@@ -0,0 +1,106 @@
+
+
+20
+
+
+let street = `27a G Cappa Estate`
+let age = 30
+let isCustomer = true
+
+console.log(street)
+console.log(age)
+console.log(true)
+
+let outcome = 100 - 50
+console.log(outcome)
+
+let totale = 100+100
+console.log(totale)
+
+let multiply = 10*10
+console.log(multiply)
+
+let division = 300/2
+console.log(division)
+
+let modulus = 11%3
+console.log(modulus)
+
+let raised2 = 2**4
+console.log(raised2)
+
+let firstprice = 50
+let secondprice = 10
+
+let totalprice = firstprice + secondprice 
+console.log(totalprice)
+
+2+2+(10*3)+10
+
+let numberofCartItems = 4
+numberofCartItems--
+console.log(numberofCartItems)
+
+
+let oldBalance = 300
+oldBalance += 700
+
+let currentBalance =1000
+currentBalance -= 500
+
+let date = "18"
+Number(date)
+console.log (typeof(date))
+
+let final = 10 < 4
+console.log(final)
+
+let start = 10 >= 20
+console.log(start)
+
+let show = prompt("Enter Your Age : Don't Lie")
+show = Number(show)
+
+let totalPrice = 1000
+let yourName = "Lindsay"
+
+let invoiceStatement = "Hello " + yourName + " Your Payment Is " + totalPrice
+console.log (invoiceStatement);
+
+let username = "Paul"
+let gameLevel = 6
+let gameresult = `Welcome ${username}, your current level is Level ${gameLevel}`
+console.log(gameresult)
+
+let roundUp = 4.6
+let resulte = Math.round(roundUp) 
+console.log(resulte);
+
+let numberTwo = 5.1
+let resultetwo = Math.ceil(numberTwo) 
+console.log(resultetwo);
+
+let numberThree = 5.9
+let resultethree = Math.floor(numberThree) 
+console.log(resultethree);
+
+
+let randomNumber = Math.random() * 10
+let rounded = Math.round(randomNumber)
+console.log(rounded);
+
+let firstStudent = 60
+let secondStudent = 84
+let thirdStudent = 52
+
+let students = Math.min(firstStudent, secondStudent, thirdStudent)
+console.log(students)
+
+let minBalance = 100
+let cardAmount = 300
+
+if(cardAmount > minBalance){
+    console.log("You are allowed to shop")
+} else{
+    console.log("Card balance is low!!!")
+}
